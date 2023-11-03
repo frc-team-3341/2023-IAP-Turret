75d6a15b3db71667b5ad227c6df24d640d85f0b7
ShawnZhuPro <shawnz05827@gmail.com>
2023-11-03 16:54:46 -0700

    Create Constants.java

---
+++ 2023-IAP-Turret/src/main/java/frc/robot/Constants.java
@@ -0,0 +1,12 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot;
+
+public final class Constants {
+    public static final class DriveTrainPorts {
+        public static final int LeftDriveTalonPort = 0; 
+        public static final int RightDriveTalonPort = 0;
+    }
+}
