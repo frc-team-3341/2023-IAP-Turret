0c2701e91837d239e6eb30ee5c002748be65b537
Yodaman07 <ayaan.irshad@gmail.com>
2023-12-10 14:57:10 -0800

    Code shown at IAP showcase

--- 2023-IAP-Turret/src/main/java/frc/robot/commands/ProtoTurret.java
+++ 2023-IAP-Turret/src/main/java/frc/robot/commands/ProtoTurret.java
@@ -2,6 +2,7 @@ package frc.robot.commands;
 
 import edu.wpi.first.math.controller.PIDController;
 import edu.wpi.first.wpilibj.XboxController;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.CommandBase;
 import frc.robot.Constants;
 import frc.robot.subsystems.PhotonVision;
@@ -30,10 +31,12 @@ public class ProtoTurret extends CommandBase {
 
     @Override
     public void initialize() {
+
 //        while (turret.getLimitValue("l")){
 //            turret.rotateTurret(0.3);
 //        }
 //        turret.rotateTurret(0);
+
 //        //rotate positive to left, negative to right
 //
 //        turret.resetEncoders();
@@ -44,27 +47,27 @@ public class ProtoTurret extends CommandBase {
 //        }
     }
 
+    public boolean checkManualControl(){
+        boolean ManualControl;
+        if (controller.getAButton()){
+            ManualControl = true;
+        }else{
+            ManualControl = false;
+        }
+        return ManualControl;
+    }
+
     @Override
     public void execute() {
-
-        if (controller.getAButtonReleased()){
-            manualControl = false;
-        }else{
-            manualControl = true;
-        }
+        turret.resetEncoders();
+        manualControl = checkManualControl();
 
         if (!manualControl) {
             while (!photonVision.targetExists()) { //All code should run while the target has not yet been found
                 while (!turret.getLimitValue("r") && !turret.getLimitValue("l")) {
-                    int setPoint = 0;
-                    if (turretSign == 1){ //one is positive --> 180 should make it go to the right while the negative value should make it go to the left
-                        setPoint = 180;
-                    }
-
-                    double calc = pid.calculate(turret.getCenterEncoderDistance(), setPoint);
-                    //turretSpeed * turretSign
-                    turret.rotateTurret(calc);
-
+                    SmartDashboard.putNumber("Speed", turretSpeed);
+                    SmartDashboard.putNumber("Sign", turretSign);
+                    turret.rotateTurret(turretSpeed * turretSign);
 
                     double angle = turret.getTicks() * 4;
                     if ((0 <= angle && angle <= threshold) || (180 - threshold <= angle && angle <= 180)) {
@@ -72,18 +75,33 @@ public class ProtoTurret extends CommandBase {
                     } else {
                         turretSpeed = 0.3;
                     }
+
+                    if (checkManualControl()){
+                        break;
+                    }
+                }
+                if (checkManualControl()){
+                    break;
                 }
                 turretSign *= -1;
+                turret.rotateTurret(turretSpeed * turretSign);
             }
-            while(photonVision.targetExists()){
-                double speed = pid.calculate(photonVision.getYaw());
-                turret.rotateTurret(-speed);
+            if(photonVision.targetExists()){
+                turret.rotateTurret(0);
+                SmartDashboard.putBoolean("Target Exists: ", photonVision.targetExists());
             }
+//            if(photonVision.targetExists()){
+//
+////                double speed = pid.calculate(photonVision.getYaw());
+////                turret.rotateTurret(-speed);
+//            }
         } else {
             double axis = controller.getRawAxis(0);
-            turret.rotateTurret(axis*1.15);
+            SmartDashboard.putNumber("axis", axis);
+            turret.rotateTurret(axis * -1.002);
         }
     }
+
 
     @Override
     public boolean isFinished() {
@@ -94,4 +112,6 @@ public class ProtoTurret extends CommandBase {
     public void end(boolean interrupted) {
         turret.rotateTurret(0);
     }
+
+
 }
